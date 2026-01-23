f68330f512ee0057a5cf28ee029e568b753a665e
Manuel Espinoza <manuelespinoza@MacBook-Air-de-Manuel.local>
2026-01-23 01:47:26 -0300

    feat/delete demo and unecessary code

--- src/components/payment/order-summary/index.ts
+++
@@ -1,9 +0,0 @@
-/**
- * Order Summary exports
- */
-
-export { OrderItemCard } from './order-item-card';
-export { OrderSummaryCard } from './order-summary-card';
-export { OrderSummarySkeleton } from './order-summary-skeleton';
-export { OrderSummaryContainer } from './order-summary-container';
-export { OrderSummaryView } from './order-summary-view';
